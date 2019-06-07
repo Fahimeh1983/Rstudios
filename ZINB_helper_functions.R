97893f66d8474bca30aa30a11b996a0ed9d9de8e
fahimehb <fahimehb@alleninstitute.org>
2019-06-07 10:25:48 -0700

    New modification for ZINB

---
+++ ZINB_helper_functions.R
@@ -0,0 +1,127 @@
+logit_fit <- function(model_name) {
+  Pi = 1
+  Alpha = NA
+  Mu = 1
+  result = list("Method" = "Logit" , "Mu" = Mu, "Alpha" = Alpha, "Pi" = Pi)
+  return(result)
+}
+
+NB_fit <- function(data.df, equation, model_name){
+  m1 <- glm.nb(equation, data = data.df)
+  Pi = 0
+  Alpha = 1. / m1$theta
+  Mu = predict(m1, type = "response")[[1]]
+  result = list("Method" = "NB" , "Mu" = Mu, "Alpha" = Alpha, "Pi" = Pi)
+  return(result)
+}
+
+ZINB_fit <- function(data.df, equation, model_name){
+  m1 <- zeroinfl(equation, data = data.df, dist = "negbin")
+  Pi <- predict(m1, type = "zero")[[1]]
+  Alpha = 1. / m1$theta
+  Mu = predict(m1, type = "count")[[1]]
+  result = list("Method" = "ZINB" , "Mu" = Mu, "Alpha" = Alpha, "Pi" = Pi)
+  return(result)
+}
+
+Fit_model <- function(data.df, gene, type_id){
+  model_name = paste0(type_id,"_", gene)
+  sub_df <- data.df[, gene]
+  if (length(sub_df) >= 10) {
+    if (all(sub_df == 0)){
+      result <- logit_fit(model_name)
+    } else if (all(sub_df != 0)){
+      eq = paste0(as.character(gene), " ~ 1")
+      result <- NB_fit(data.df, equation = as.formula(eq), model_name)
+    } else {
+      eq = paste0(as.character(gene), " ~ 1")
+      result <- try(ZINB_fit(data.df, equation = as.formula(eq), model_name))
+      if("try-error" %in% class(result)) {
+        print(c("Was not able to fit ZINB for gene:", gene, "Zero%", sum(sub_df == 0) / length(sub_df) ))
+        result <- logit_fit(model_name)
+      }
+    }
+    return(result)
+  } else {
+    result = list("Method" = "Not enough data" , "Mu" = NA, "Alpha" = NA, "Pi" = NA)
+    return(result)
+  }
+}
+
+Get_contamination_factor <- function(cell_name, first_type, second_type){
+  Beta_list[[c]][Beta_list[[c]][,"Type1"] == first_type & Beta_list[[c]][,"Type2"] == second_type,][,"B"]
+}
+
+Get_contamination_counts <- function(beta, yg){
+  contamin_count1 <- list()
+  contamin_count2 <- list()
+  for (k in 0:yg){
+    contamin_count1 <- c(contamin_count1, round(k / beta))
+    contamin_count2 <- c(contamin_count2, round(( yg - k) / (1 - beta)))
+  }
+  result <- list("contamin_count1" = contamin_count1 , "contamin_count2"= contamin_count2)
+  return(result)
+}
+
+
+
+Compute_pyg <- function(gene, yg, list1, list2, cell_name, first_type, second_type, count_threshold) {
+    beta <- Get_contamination_factor(cell_name, first_type, second_type)
+    con <- Get_contamination_counts(beta, yg)
+    con1 <- unlist(con$contamin_count1) 
+    con2 <- unlist(con$contamin_count2) 
+    if (max(con1) > 2 * count_threshold | max(con2) > 2 * count_threshold) {
+      return(0)
+    } else {
+      term1 <- unlist(Compute_likelihood_efficient(con1, Method = list1[[gene,"Method"]], Pi = list1[[gene,"Pi"]], Mu = list1[[gene,"Mu"]], Alpha = list1[[gene,"Alpha"]]))
+      term2 <- unlist(Compute_likelihood_efficient(con2, Method = list2[[gene,"Method"]], Pi = list2[[gene,"Pi"]], Mu = list2[[gene,"Mu"]], Alpha = list2[[gene,"Alpha"]]))
+      return(sum(term1 * term2))
+    }
+  
+}
+
+Compute_pure_pyg<- function(gene, yg, list, cell_name, first_type){
+  unlist(Compute_likelihood_efficient(yg, Method = list[[gene,"Method"]], Pi = list[[gene,"Pi"]], Mu = list[[gene,"Mu"]], Alpha = list[[gene,"Alpha"]]))
+}
+
+Compute_likelihood_efficient <- function(contam_list, Method, Mu, Pi, Alpha) {
+  if(Method == "NB"){
+    #print("I am here1")
+    #print(contam_list)
+    n <- 1/Alpha
+    prob <- n /(n + Mu)
+    L <- list()
+    for (x in contam_list){
+      L <- c(L, dnbinom(x, n, prob, log = FALSE))
+    }
+  } else if (Method == "ZINB") {
+    #print("I am here2")
+    #print(contam_list)
+    n <- 1/Alpha
+    prob <- n /(n + Mu)  
+    L <- list()
+    for (x in contam_list){
+      if (x == 0) {
+        LNB0 <- Pi + (1 - Pi) * (1 + Alpha * Mu) ^ (-1 / Alpha)
+        L <- c(L, LNB0)
+      } else {
+        LNB <- dnbinom(x, n, prob, log = FALSE)
+        LNB <- (1 - Pi) * LNB
+        L <- c(L, LNB)
+      }
+    }
+    
+  } else if (Method == "Logit") {
+    #print("I am here3")
+    #print(contam_list)
+    L <- list()
+    for (x in contam_list) {
+      if (x == 0){
+        L <- c(L, 1)
+      } else {
+        L <- c(L, 0)
+      }
+    }
+  }
+  return(L)
+}
--- ZINB_percell.R
+++ ZINB_percell.R
@@ -1,3 +1,11 @@
+.libPaths("/home/fahimehb/R/x86_64-redhat-linux-gnu-library/3.5")
+FIT_ZINB =  FALSE 
+COMPUTE_BETA = TRUE 
+COMPUTE_LIKELIHOOD = TRUE 
+AGGREGATE_RESULTS = TRUE
+args = commandArgs(trailingOnly=TRUE)
+run_iter = as.numeric(args[1]) 
+
 ##########################################################################################
 ### Reading the counts: ##################################################################
 ##########################################################################################
@@ -15,64 +23,65 @@ library(scrattch.io)
 
 work.dir = "/allen/programs/celltypes/workgroups/rnaseqanalysis/Fahimehb/patchseq-work-dir/Patchseq_vs_FACs_cre_analysis/mouse_patchseq_VISp_20181220_collapsed40_cpm/"
 tome <- "//allen/programs/celltypes/workgroups/rnaseqanalysis/shiny/tomes/facs/mouse_V1_ALM_20180520/transcrip.tome"
-markers.path <- "/allen/programs/celltypes/workgroups/rnaseqanalysis/shiny/Taxonomies/AIT2.3.1/select.markers.rda"
+#markers.path <- "/allen/programs/celltypes/workgroups/rnaseqanalysis/shiny/Taxonomies/AIT2.3.1/select.markers.rda"
+markers.path <- paste0(work.dir, "/1000_markers.rda")
 
 # Sample annotations
 # Will throw Warnings, but these are OK - just because of how NAs are stored in HDF5 files.
-FACS.anno <- read_tome_anno(tome)
+#FACS.anno <- read_tome_anno(tome)
 # Read all counts as sparse matrix
 # These are stored in samples (rows) x genes (columns) format
-exon_counts <- read_tome_dgCMatrix(tome = tome, target = "/data/exon")
-intron_counts <- read_tome_dgCMatrix(tome = tome, target = "/data/intron")
-FACS.counts <- exon_counts + intron_counts
-colnames(FACS.counts) <- read_tome_gene_names(tome)
-rownames(FACS.counts) <- read_tome_sample_names(tome)
+#exon_counts <- read_tome_dgCMatrix(tome = tome, target = "/data/exon")
+#intron_counts <- read_tome_dgCMatrix(tome = tome, target = "/data/intron")
+#FACS.counts <- exon_counts + intron_counts
+#colnames(FACS.counts) <- read_tome_gene_names(tome)
+#rownames(FACS.counts) <- read_tome_sample_names(tome)
 # See everything stored in the tome
 #h5ls(tome)
-print("Done!")
-
+#print("Done!")
 
 load(markers.path)
+print("Done loading markers list!!")
 #Long_markers_list <- select.markers
-Long_markers_list <- sample(select.markers, 1000)
-Long_markers_list[grepl( "Rik" , Long_markers_list) ]  <- paste0("rename",Long_markers_list[grepl( "Rik" , Long_markers_list) ])
-Long_markers_list <- gsub("-", "_", Long_markers_list)
-rm(select.markers)
-short_markers_list <- c("Npy", "Npy1r", "Npy2r", "Npy5r",
+#Long_markers_list <- sample(select.markers, 500)
+#Long_markers_list[grepl( "Rik" , Long_markers_list) ]  <- paste0("rename",Long_markers_list[grepl( "Rik" , Long_markers_list) ])
+#Long_markers_list <- gsub("-", "_", Long_markers_list)
+#rm(select.markers)
+short_markers_list <- c("Npy", "Npy1r", "Npy2r", "Npy5r", 
                         "Sst" ,"Sstr1", "Sstr2", "Sstr3", "Sstr4", "Cort",
                         "Vip", "Vipr1", "Vipr2",
-                        "Tac2", "Tacr3",
-                        "Cck", "Cckbr",
-                        "Penk", "Oprd1", "Oprm1",
+                        "Tac2", "Tacr3", 
+                        "Cck", "Cckbr", 
+                        "Penk", "Oprd1", "Oprm1", 
                         "Crh", "Crhr1", "Crhr2",
-                        "Tac1", "Tacr1",
-                        "Pdyn", "Oprk1",
+                        "Tac1", "Tacr1", 
+                        "Pdyn", "Oprk1", 
                         "Pthlh","Pth1r",
                         "Pnoc", "Oprl1",
-                        "Trh", "Trhr", "Trhr2",
+                        "Trh", "Trhr", "Trhr2", 
                         "Grp", "Grpr",
-                        "Rln1", "Rxfp1", "Rxfp2", "Rxfp3",
-                        "Adcyap1", "Adcyap1r1",
-                        "Nts", "Ntsr1", "Ntsr2",
+                        "Rln1", "Rxfp1", "Rxfp2", "Rxfp3", 
+                        "Adcyap1", "Adcyap1r1", 
+                        "Nts", "Ntsr1", "Ntsr2", 
                         "Nmb", "Nmbr")
 
 
 #Removing all low quality cells from FACS data
 LowQ_types <- c("Low Quality VISp L5 PT Ctxn3 2", "Batch Grouping VISp L5 PT Chrna6",
                 "Batch Grouping VISp L5 PT Ctxn3", "Low Quality VISp L6 CT Ptprt_2",
-                "Low Quality VISp L5 PT Ctxn3 1", "Doublet SMC and Glutamatergic",
-                "Doublet Astro Aqp4 Ex", "Low Quality ALM L6 CT Cpa6",
+                "Low Quality VISp L5 PT Ctxn3 1", "Doublet SMC and Glutamatergic", 
+                "Doublet Astro Aqp4 Ex", "Low Quality ALM L6 CT Cpa6", 
                 "Low Quality Meis2 Adamts19 ", "Doublet Endo and Peri_1",
-                "Doublet VISp L5 NP and L6 CT", "Low Quality Sst Chodl",
-                "Low Quality Astro Aqp4" , "Doublet Endo Peri SMC",
+                "Doublet VISp L5 NP and L6 CT", "Low Quality Sst Chodl", 
+                "Low Quality Astro Aqp4" , "Doublet Endo Peri SMC", 
                 "Low Quality L4 Rspo1", "High Intronic VISp L5 Endou",
                 "Low Quality VISp L6 CT Ptprt_1")
 
-save(FACS.anno,file = paste0(work.dir, "/Counts/FACS_anno.rda"))
-save(FACS.counts,file = paste0(work.dir, "/Counts/FACS_counts.rda"))
-print("Done saving FACS.anno!!")
-#load(paste0(work.dir, "/Counts/FACS_anno.rda"))
-#load(paste0(work.dir, "/Counts/FACS_counts.rda"))
+#save(FACS.anno,file = paste0(work.dir, "/Counts/FACS_anno.rda"))
+#save(FACS.counts,file = paste0(work.dir, "/Counts/FACS_counts.rda"))
+#print("Done saving FACS.anno!!")
+load(paste0(work.dir, "/Counts/FACS_anno.rda"))
+load(paste0(work.dir, "/Counts/FACS_counts.rda"))
 select.cl <- setdiff(unique(FACS.anno$cluster_label), LowQ_types)
 FACS.anno <- FACS.anno[FACS.anno$cluster_label %in% select.cl,]
 rownames(FACS.anno) <- FACS.anno$sample_name
@@ -83,8 +92,7 @@ colnames(FACS.counts) <- gsub("-", "_", colnames(FACS.counts))
 FACS.counts <- FACS.counts[FACS.cells,Long_markers_list]
 df <- cbind(FACS.anno$cluster_id, as.data.frame.matrix(FACS.counts))
 colnames(df) <- c("Type", colnames(FACS.counts))
-dim(df)
-
+print(dim(df))
 ##########################################################################################
 ### Some initialization: #################################################################
 ##########################################################################################
@@ -98,43 +106,43 @@ for (t in Good_types) {
 }
 #genes <- short_markers_list
 genes <- Long_markers_list
-save(Long_markers_list, file = paste0(work.dir,"/1000_markers.rda"))
+print("Done initialization!!!")
 ##########################################################################################
 ### fit ZINB or NB or logit per gene per Type: ###########################################
 ##########################################################################################
 
 source("/allen/programs/celltypes/workgroups/rnaseqanalysis/Fahimehb/git_workspace/Rstudios/Rstudios/ZINB_helper_functions.R")
-
-All_fit_values <- list()
-for (t in Good_types) {
-  start_time = Sys.time()
-  print(c("Type:", t))
-  new_df <- df[df$Type == t ,]
-  Fit_values <- tapply(1:nrow(new_df), as.character(new_df$Type), function(x) sapply(colnames(new_df)[-1], function(g)Fit_model(new_df[x,], g, t)))
-  All_fit_values <- c(All_fit_values, Fit_values)
-  end_time = Sys.time()
-  print(end_time - start_time)
-}
-
-save(All_fit_values, file=paste0(work.dir,"/1000_fit_values.rda"))
+if (FIT_ZINB){
+  All_fit_values <- list()
+  for (t in Good_types) {
+    print(c("Type:", t))
+    new_df <- df[df$Type == t ,]
+    Fit_values <- tapply(1:nrow(new_df), as.character(new_df$Type), function(x) sapply(colnames(new_df)[-1], function(g)Fit_model(new_df[x,], g, t)))
+    All_fit_values <- c(All_fit_values, Fit_values)
+  }
+  save(All_fit_values, file=paste0(work.dir,"/All_fit_values.rda"))
+} else {
+load(paste0(work.dir,"/1000_fit_values.rda")) 
+} 
 df <- t(df)
 
 ##########################################################################################
 ### Setting RUN_ITER: ####################################################################
 ##########################################################################################
-#run_iter <- 5000
-Beta_list <- list()
-Finaldf <- list()
 
-for (run_iter in 4:5){
-  ##########################################################################################
-  ### Find Beta for each pair of cell type using an optimizer: #############################
-  ##########################################################################################
-  
-  Err <- function(data, par) {
-    with(data, sum((par * term1 +  term2)^2))
-  }
-  
+#run_iter <- 3000
+
+##########################################################################################
+### Find Beta for each pair of cell type using an optimizer: #############################
+##########################################################################################
+
+Err <- function(data, par) {
+  with(data, sum((par * term1 +  term2)^2))
+}
+
+Beta_file_name = paste0(FACS.cells[run_iter],"_Beta.rda")
+if (COMPUTE_BETA){
+  Beta_list <- list()
   for (c in FACS.cells[run_iter]){
     B <- list()
     p1 <- list()
@@ -156,19 +164,19 @@ for (run_iter in 4:5){
         Pi1 <- unlist(list1[genes, "Pi"])
         Pi2 <- unlist(list2[genes, "Pi"])
         term1 <- Mu1 - Mu1 * Pi1 - Mu2 + Mu2 * Pi2
-        term2 <- Mu2 - Mu2 * Pi2 - y 
+        term2 <- Mu2 - Mu2 * Pi2 - y
         dat <- as.data.frame(cbind(term1, term2))
-        results <- optim(par = runif(1, 0.09, 0.9), 
-                         fn = Err, 
-                         data = dat, 
-                         method = "Brent", 
-                         lower = 0.01, 
-                         upper = 0.99)
+        results <- optim(par = runif(1, 0.09, 0.9),
+                       fn = Err,
+                       data = dat,
+                       method = "Brent",
+                       lower = 0.01,
+                       upper = 0.99)
         B <- c(B, results$par)
       }
       p1 <- c(p1, first_type)
       p2 <- c(p2, second_type)
-    } 
+    }
     temp <- cbind.data.frame(unlist(B), unlist(p1), unlist(p2))
     colnames(temp) <- c("B", "Type1", "Type2")
     Beta_list[[c]] <- temp
@@ -176,17 +184,19 @@ for (run_iter in 4:5){
     print(end_time - start_time)
   }
   print("Done!")
-  
-  Beta_file_name = paste0(FACS.cells[run_iter],"_Beta.rda")
   save(Beta_list, file=paste0(work.dir, "/Beta_files/",Beta_file_name))
-  
-  ##########################################################################################
-  ### Find the probability of the observation given Beta: ##################################
-  ##########################################################################################
-  
-  source("/allen/programs/celltypes/workgroups/rnaseqanalysis/Fahimehb/git_workspace/Rstudios/Rstudios/ZINB_helper_functions.R")
-  
-  count_threshold <- max(df)
+} else {load(paste0(work.dir, "/Beta_files/",Beta_file_name))}
+
+
+##########################################################################################
+### Find the probability of the observation given Beta: ##################################
+##########################################################################################
+
+source("/allen/programs/celltypes/workgroups/rnaseqanalysis/Fahimehb/git_workspace/Rstudios/Rstudios/ZINB_helper_functions.R")
+
+pyg_file_name = paste0(FACS.cells[run_iter],"_pyg.rda")
+count_threshold <- max(df)
+if (COMPUTE_LIKELIHOOD){
   pyg <- list()
   for (c in FACS.cells[run_iter]){
     y <- df[genes,c]
@@ -221,17 +231,19 @@ for (run_iter in 4:5){
     end_time = Sys.time()
     print(end_time - start_time)
   }
-  
-  #pp[which.max(unlist(l))]
-  #FACS.anno[[FACS.cells[run_iter], c( "cluster_id")]]
-  #unlist(pp)[order(unlist(l))]
-  ##########################################################################################
-  ### Find the probability of the observation given Beta: ##################################
-  ##########################################################################################
-  
-  cluster_lable_id <- as.data.frame(unique(FACS.anno[,c("cluster_label", "cluster_id")]))
-  cell <- FACS.cells[run_iter]
+  save(pyg, file=paste0(work.dir, "/Probability_files/",pyg_file_name))
+} else {load(paste0(work.dir, "/Probability_files/",pyg_file_name))}
 
+
+##########################################################################################
+### Find the probability of the observation given Beta: ##################################
+##########################################################################################
+
+cluster_lable_id <- as.data.frame(unique(FACS.anno[,c("cluster_label", "cluster_id")]))
+cell <- FACS.cells[run_iter]
+
+if (AGGREGATE_RESULTS){
+  Finaldf <- list()
   py <- list()
   for (c in cell){
     Type1 <- list()
@@ -255,29 +267,15 @@ for (run_iter in 4:5){
     Finaldf[[c]] <- merge(py[[c]], Beta_list[[c]], by=c("Type1","Type2"))
     Finaldf[[c]][["cluster_id"]] <- FACS.anno[[cell, c( "cluster_id")]]
     Finaldf[[c]][["cluster_label"]] <- FACS.anno[[cell, c( "cluster_label")]]
-    Finaldf[[c]]["pair_identity"] <- ifelse(Finaldf[[c]][,"Type1"] == Finaldf[[c]][,"cluster_id"] |
-                                              Finaldf[[c]][,"Type2"] == Finaldf[[c]][,"cluster_id"] , "U_or_V", "None")
+    Finaldf[[c]]["pair_identity"] <- ifelse(Finaldf[[c]][,"Type1"] == Finaldf[[c]][,"cluster_id"] |  Finaldf[[c]][,"Type2"] == Finaldf[[c]][,"cluster_id"] , "U_or_V", "None") 
   }
-} 
-
-
-#is U and V contamination symmetric?!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-##########################################################################################
-### Find the probability of the observation given Beta: ##################################
-##########################################################################################
-
-output <- list()
-for (run_iter in 1:650){
-  print(run_iter)
-  select.cell <- FACS.cells[run_iter]
-  load(paste0(work.dir, "ZINB_results_files/", FACS.cells[run_iter],"_results.rda"))
-  output[[select.cell]] <- Finaldf[[select.cell]]
+  aggregated_results_file_name = paste0(FACS.cells[run_iter],"_results.rda")
+  save(Finaldf, file=paste0(work.dir, "/ZINB_results_files/",aggregated_results_file_name))
 }
 
-run_iter = 369
-select.cell <- FACS.cells[run_iter]
-ggplot(output[[select.cell]][!is.na(output[[select.cell]][,"logp"]),], aes(logp, B, colour = pair_identity)) + 
-  geom_point(alpha = 0.4) + xlab("Log(p)") + ylab("B")
 
-output[[select.cell]][order(output[[select.cell]][,"logp"], decreasing = TRUE),][1:10,]
+#select.cell <- FACS.cells[run_iter]
+#ggplot(Finaldf[[select.cell]][!is.na(Finaldf[[select.cell]][,"logp"]),], aes(logp, B, colour = pair_identity)) + 
+#  geom_point(alpha = 0.4) + xlab("Log(p)") + ylab("B")
 
+#Finaldf[[select.cell]][order(Finaldf[[select.cell]][,"logp"], decreasing = TRUE),][1:10,]
